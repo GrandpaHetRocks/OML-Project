6e050b87859440160fc50b820b592fa8688be63a
Ayush MS <ayushms0209@gmail.com>
2021-11-09 17:45:51 +0530

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -59,11 +59,11 @@ class Net(nn.Module):
         #x=self.quant(x)
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
+        # x = F.relu(self.conv2(x))
+        # x = F.max_pool2d(x, 2, 2)
         x = x.view(-1, 4*4*10)
         x = F.relu(self.fc1(x))
-        x = self.fc1(x)
+        # x = self.fc2(x)
         return F.log_softmax(x, dim=1)
     
 client={}
