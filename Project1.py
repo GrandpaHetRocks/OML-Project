30037a90e91d09c65704b55097b9131e69cb8960
Ayush MS <ayushms0209@gmail.com>
2021-11-09 17:49:15 +0530

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -50,8 +50,8 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         #self.quant = torch.quantization.QuantStub()
-        self.conv1 = nn.Conv2d(1, 5, 5, 1)
-        # self.conv2 = nn.Conv2d(5, 10, 5, 1)
+        self.conv1 = nn.Conv2d(1, 2, 5, 1)
+        self.conv2 = nn.Conv2d(2, 3, 5, 1)
         self.fc1 = nn.Linear(4*4*10, 10) #noniid:50 iid:5
         self.fc2 = nn.Linear(10, 10) #50
 
@@ -59,8 +59,8 @@ class Net(nn.Module):
         #x=self.quant(x)
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
-        # x = F.relu(self.conv2(x))
-        # x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
         x = x.view(-1, 4*4*10)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
