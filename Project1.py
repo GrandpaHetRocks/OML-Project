a177603c025ca0556d9f9b366d380c6292c6b712
Ayush MS <ayushms0209@gmail.com>
2021-11-09 18:18:44 +0530

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -43,7 +43,7 @@ logps = model(images)
 loss = criterion(logps, labels)
 
 
-optimizer = optim.Adam(model.parameters(), lr=0.1)
+optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 epochs = 2
 ac=[]
