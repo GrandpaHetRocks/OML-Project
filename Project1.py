6942846da1f06f80aaae58bf9b36896cf5d0cee2
Ayush MS <ayushms0209@gmail.com>
2021-11-11 19:03:51 +0530

    --

--- Project1.py
+++ Project1.py
@@ -44,7 +44,7 @@ logps = model(images)
 loss = criterion(logps, labels)
 
 
-optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999))
+optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 epochs = 100
 ac=[]
