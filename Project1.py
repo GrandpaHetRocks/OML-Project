4a76ad1325f807b8ec1c4c9aeeb202a78313b16a
Ayush MS <ayushms0209@gmail.com>
2021-11-12 16:36:06 +0530

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -44,7 +44,7 @@ logps = model(images)
 loss = criterion(logps, labels)
 
 
-optimizer = optim.Adagrad(model.parameters(), lr=0.01, eps=1e-10)
+optimizer = optim.Adagrad(model.parameters(), lr=0.01, eps=1e-8)
 
 epochs = 100
 ac=[0]
